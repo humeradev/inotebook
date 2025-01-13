8367e90767ea45d6e3d6fffd580bb4e72517583a
Humera Khan <94207149+humeradev@users.noreply.github.com>
2025-01-13 09:57:58 +0000

    Added user schema and checked it from database

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,18 @@ connectToMongo();
 const app = express()
 const port = 3006
 
+
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+//my available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const User = require('../models/User');
+
+const router = express.Router();
+
+
+//Create a user : using POST ".api/auth/" .dose not requrew auth
+
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    
+    res.send(req.body);
+})
+
+
+module.exports = router
